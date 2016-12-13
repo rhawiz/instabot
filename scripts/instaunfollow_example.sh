f8b406a74650c557347aa93a1b36d2ba434a7673
rawand <r.hawiz@geophy.com>
2016-12-13 19:03:06 +0000

    fixed example scripts

--- scripts/instaunfollow_example.sh
+++ scripts/instaunfollow_example.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo python instaunfollow.py --username hwzearth --password hidden --rate 100 --wait 60,90 --unfollow_all n
+sudo python instaunfollow.py --username hwzearth --password hidden --rate 100 --wait 60,90 --unfollow_all
