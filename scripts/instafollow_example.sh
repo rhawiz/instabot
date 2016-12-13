5787ba7889ea0c3ceef706b786bd921310cee989
rawand <r.hawiz@geophy.com>
2016-12-13 19:02:43 +0000

    wait input -> min,max (e.g. 60,90)
    instaunfollow implemented

---
+++ scripts/instafollow_example.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+sudo python instafollow.py --username fiftytwofood --password hidden --rate 100 --wait 60,90 --similar_users love_food,flavorgod,sobeautifullyreal,inspiralized,dessert__lover,insiderdessert,foodfast_,cakes,buzzfeedfood,foodnetwork,spoonforkbacon
--- scripts/run_hwzearth.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-sudo python instafollow.py --username hwzearth --password hidden --rate 100 --wait 60 --similar_users awesome.earth,nasa,bbcearth,earthporm,usinterior,paulnicklen,jimmy_chin,timlaman
