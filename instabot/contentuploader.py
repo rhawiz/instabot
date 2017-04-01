31f948b22185856f03545c10263f9e9c1d3ecd61
Rawand Hawiz <r.hawiz@geophy.com>
2017-04-01 17:04:02 +0200

    flask content uploader

--- instabot/contentuploader.py
+++ instabot/contentuploader.py
@@ -23,6 +23,9 @@ def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
+@app.route('/', methods=['GET'])
+def home():
+    return "Instabot"
 
 @app.route('/upload', methods=['GET', 'POST'])
 def upload_file():
---
+++ wsgi.py
@@ -0,0 +1,4 @@
+from instabot.contentuploader import app
+
+if __name__ == "__main__":
+    app.run()
