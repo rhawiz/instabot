59a74970235932a14f3ca4dfea8f585c9e67c62e
Rawand Hawiz <r.hawiz@geophy.com>
2017-05-04 00:20:50 +0200

    deploy.sh

--- instabot/deploy.sh
+++ instabot/deploy.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 
-sudo chown -R rawandhawiz:rawandhawiz ..
-python contentuploader.py &
-ngrok http -log=stdout 5000 > /dev/null &
+user="$USER"
+
+echo "Attempting to kill existing processes..."
+pkill -f "python contentuploader.py"
+pkill -f "ngrok"
+
+echo "Changing permissions to $user:$user..."
+sudo chown -R $user:$user ..
+echo "Running content uploader..."
+nohup python contentuploader.py &
+echo "Running ngrok..."
+nohup ngrok http -region=eu -log=stdout 5000 > /dev/null &
+sleep 1
 python ngrokinstances.py
