0361bf3f5945e827228aec056e3226f826dcc020
Rawand Hawiz <r.hawiz@geophy.com>
2017-05-03 23:48:34 +0200

    deploy.sh, ngrok

--- instabot/deploy.sh
+++ instabot/deploy.sh
@@ -4,4 +4,5 @@ sudo chown -R rawandhawiz:rawandhawiz ..
 sudo chown root:root ngrok
 sudo chmod +x ngrok
 python contentuploader.py &
-./ngrok http 5000 &
+ngrok http -log=stdout 5000 > /dev/null &
+python ngrokinstances.py
