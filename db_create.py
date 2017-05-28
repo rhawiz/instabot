4f278c2f1706709efa3775831aa299722becff5a
Rawand Hawiz <r.hawiz@geophy.com>
2017-05-28 20:41:21 +0200

    deploy script

--- db_create.py
+++ db_create.py
@@ -1,11 +1,4 @@
 #!flask/bin/python
-from migrate.versioning import api
-from config import Config as cfg
+
 from app import db
-import os.path
 db.create_all()
-if not os.path.exists(cfg.SQLALCHEMY_MIGRATE_REPO):
-    api.create(cfg.SQLALCHEMY_MIGRATE_REPO, 'database repository')
-    api.version_control(cfg.SQLALCHEMY_DATABASE_URI, cfg.SQLALCHEMY_MIGRATE_REPO)
-else:
-    api.version_control(cfg.SQLALCHEMY_DATABASE_URI, cfg.SQLALCHEMY_MIGRATE_REPO, api.version(cfg.SQLALCHEMY_MIGRATE_REPO))
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo python db_create.py
+sudo service apache2 restart
