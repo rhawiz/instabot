db846ae0239c9552591ef8d783f1b0e916e61dc1
Rawand Hawiz <r.hawiz@geophy.com>
2017-08-05 13:48:26 +0200

    udated instabot.conf

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,6 @@ sudo git pull
 sudo rm instabot.db
 python db_create.py
 sudo chmod 664 instabot.db
+sudo cp instabot.conf /etc/apache2/sites-available/
+sudo a2ensite instabot.conf
 sudo service apache2 restart
